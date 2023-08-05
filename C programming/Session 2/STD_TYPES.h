f3c9850e45bf21ab79a7f44c65e5221afd8b8e95
mhmdessam94 <me8705219@gmail.com>
2023-08-05 19:53:52 +0300

    added file typesdef

---
+++ C programming/Session 2/STD_TYPES.h
@@ -0,0 +1,9 @@
+
+#typedef unsigned char uchar8;
+#typedef char char8;
+#typedef unsigned short int uint16;
+#typedef short int int16;
+#typedef long int int36;
+#typedef unsigned long int uint36;
+#typedef float float36;
+#typedef double double64;
